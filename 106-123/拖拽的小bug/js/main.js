e965d31d0d7efd50cdba4d8435f9f8f9ad7dfab3
Ling Ding <azusakaworu@gmail.com>
2019-01-18 20:28:58 -0500

    偏移量和拖拽

--- 106-123/拖拽的小bug/js/main.js
+++ 106-123/拖拽的小bug/js/main.js
@@ -1,6 +1,6 @@
 //js
 
-/*拖拽练习
+/*拖拽练习--一些BUG
 
 .onmousemove
 .onmousedown
@@ -10,15 +10,19 @@
 ---------*/
 
 var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var img1 = document.getElementById("img1");
+
+function drag(obj){
 
 
 //1.鼠标按下时
-box1.onmousedown= function(event){//这个事件对象范围大
+obj.onmousedown= function(event){//这个事件对象范围大
 	console.log('鼠标onmousedown按下了~~');
   //div的水平偏移量 = 鼠标.clientX - 元素.offsetLeft
   //div的垂直偏移量 = 鼠标.clientY - 元素.offsetTop
-   var ol = event.clientX- box1.offsetLeft;
-   var ot = event.clientY - box1.offsetTop;
+   var ol = event.clientX- obj.offsetLeft;
+   var ot = event.clientY - obj.offsetTop;
     
 
 	//2.跟随鼠标移动
@@ -27,8 +31,8 @@ box1.onmousedown= function(event){//这个事件对象范围大
 		var x = event.pageX - ol; //减去div的水平偏移量
 		var y = event.pageY - ot; //减去div的垂直偏移量
 
-	box1.style.left = x +"px";
-	box1.style.top = y +"px";
+	obj.style.left = x +"px";
+	obj.style.top = y +"px";
 
 	};
 
@@ -40,5 +44,11 @@ document.onmouseup = function(){
   //取消document的onmouseup事件
    document.onmouseup = null;//变成一次性事件
 };
-	
+	return false;//全选 ctrl + A 以后 box不会有异常
 };
+
+}
+
+drag(box1);
+drag(box2);
+drag(img1);
